708ac110d2fbe12f4444b137fd774b451e3c37c2
Anna <Annalaora1@gmail.com>
2022-08-30 12:22:23 +0200

    Page panier

---
+++ front/js/elementHTML.js
@@ -0,0 +1,99 @@
+// ajoute une classe à un élément html s'il est défini
+function addClassName( element, classHtml) {
+  // si le paramètre classHTML est défini alors on ajoute une classe à l'élément
+  if(classHtml != "") {
+      // ajout de la classe html
+      element.className = classHtml;
+      
+  }
+
+  return element;
+}
+
+
+
+// créé un élément html et l'ajoute à son parent
+function createElementHTML(parent, name, classHTML = "") {
+   // création d'une balise html qui porte le nom "name" (exemple : h3, a, div, p, etc...)
+   let element = document.createElement(name);
+
+   // ajoute une class css à l'élément
+   element = addClassName(element, classHTML);
+
+   // 
+   // parent.innerHTML = element;
+   
+   parent.append(element);
+
+   return element;
+}
+
+function createDiv(parent, classHTML = "") {
+ 
+    return createElementHTML(parent, "div", classHTML);
+}
+
+function createOption(parent, value, content, classHTML ="") {
+  
+  let option = createElementHTML(parent, "option", classHTML);
+
+  option.value = value;
+  option.innerHTML = content;
+
+  return option;
+
+}
+
+
+function createLink(parent, href, classHtml = "") {
+   
+    // création d'une balise html <a> et ajout au parent
+    let link = createElementHTML(parent, "a", classHtml);
+  
+    
+    // attribution du lien
+    link.href = href;
+
+    return link;
+  }
+
+  function createArticle(parent, classHtml = "") {
+    // création d'une blaise html <article>
+    let article =  createElementHTML(parent, "article", classHtml);
+  
+    return article;
+  }
+
+  function createImage(parent, src, alt, classHtml = "") {
+    // création d'une blaise html <img>
+    let image = createElementHTML(parent, "img", classHtml);
+    
+    // définition de l'attribut src
+    image.src = src;
+    // définition de l'attribut alt
+    image.alt = alt;
+
+    return image;
+  }
+
+  function createH3(parent, title, classHtml = "") {
+    // création d'une blaise html <h3>
+    let h3 = createElementHTML(parent, "h3", classHtml);
+    
+   
+    // définition du contenu du titre
+    h3.innerHTML = title;
+
+    return h3;
+  }
+  
+  function createParagraphe(parent, content, classHtml = "") {
+    // création d'une blaise html <p>
+    let paragraphe = createElementHTML(parent, "p", classHtml);
+
+    paragraphe.innerHTML = content;
+
+    return paragraphe;
+  }
+
+
--- front/js/produits.js
+++ front/js/produits.js
@@ -1,25 +1,97 @@
-//récupération de la chaîne de requête dans l'url
-const queryString_url_id = window.location.search;
-console.log(queryString_url_id);
+const url = "http://localhost:3000/api/products";
 
-// méthode pour extrair_ l'id
-const urlSearchParams = new URLSearchParams(queryString_url_id);
-console.log(urlSearchParams);
+// créé une carte produit et l'ajoute à son élément parent
 
-const id = urlSearchParams.get("id");
-console.log(id);
+function createCarteProduit(parent, produit) {
+  // création du lien et ajout au body
+  let carteProduit = createLink(parent, "./product.html?id=" + produit._id);
 
-//affichage du produit (de l'objet) qui a été sélectionné par l'id
-//---Méthode 2 : avec fetch et en mettent la valeur de l'id à la fin de l'url
-let response = fetch(`http://localhost:3000/api/products/${id}`);
-console.log(response);
+  // creation de l'article et ajoute de l'article au lien
+  let articleProduit = createArticle(carteProduit);
 
-//sélection de la classe où je vais injecter le code HTML
-const poisitionElement = document.querySelector(".item__img");
-console.log(poisitionElement);
+  // création de l'image et ajout à l'article
+  createImage(articleProduit, produit.imageUrl, produit.altTxt);
 
-//structure HTMl pour l'affichage du produit sélectionné
-const structureProduits = `<img src="${response.imageUrl}" alt="${response.altTxt}">`;
+  // création du titre du produit et ajout à l'article
+  createH3(articleProduit, produit.name, "productName");
 
-//injecttion HTML dans la page produit
-poisitionElement.innerHTML = poisitionElement;
+  // création de la description du produit et ajout à l'article
+  createParagraphe(articleProduit, produit.description, "productDescription");
+
+  // retourne l'élément html créé
+  return carteProduit;
+}
+
+function getAllProduits() {
+  let cardsFetch = function () {
+    //fonction qui va faire la requete get avec fetch
+    fetch(url)
+      .then((response) => response.json()) //reponse avec message dans la console
+      .then((data) => {
+        let produitsSection = document.getElementById("items"); //variable qui se referet a la balise section dans le html
+
+        for (i = 0; i < data.length; i++) {
+          // ici c'est la boucle for...of qui va afficher les produits
+
+          createCarteProduit(produitsSection, data[i]); // .innerHTML va mettre cartes Produits dans produits section mais dans le HTML
+        }
+      })
+      .catch((err) => console.log("Erreur:" + err)); //message si il y a une erreurs
+  };
+  cardsFetch();
+}
+
+function getIdUrl() {
+  //récupération de la chaîne de requête dans l'url
+  const queryString_url_id = window.location.search;
+
+  // méthode pour extrair_ l'id
+  const urlSearchParams = new URLSearchParams(queryString_url_id);
+
+  let id = urlSearchParams.get("id");
+
+  return id;
+}
+
+function createImageProduit(parent, produit, classHTML = "") {
+  return createImage(parent, produit.imageUrl, produit.altTxt, classHTML);
+}
+
+function createDetailProduit(produit) {
+  // récupération de l'élément ou on va ajouter l'image du produit
+  let item_img = document.querySelector(".item__img");
+
+  // création de l'image et ajout à div item__img
+  createImageProduit(item_img, produit);
+
+  // récupération de l'élément html où on va mettre la description
+  let description = document.querySelector("#description");
+
+  // ici pas besoin de créer un élément html, on ajoute directement le texte au paragraphe
+  description.innerHTML = produit.description;
+
+  // récupération de l'élément html où on va ajouter une nouvelle option
+  let colors = document.querySelector("#colors");
+
+  // ajout des options dans la liste déroulante
+  for (i = 0; i < produit.colors.length; i++) {
+    createOption(colors, produit.colors[i], produit.colors[i]);
+  }
+}
+
+// récupère un produit selon son id passé dans l'url et l'insère dans la page
+function getProduitById() {
+  let id = getIdUrl();
+
+  let cardFetch = function () {
+    //fonction qui va faire la requete get avec fetch
+    fetch(url + "/" + id)
+      .then((response) => response.json()) //reponse avec message dans la console
+      .then((leProduit) => {
+        createDetailProduit(leProduit);
+      })
+      .catch((err) => console.log("Erreur:" + err)); //message si il y a une erreurs
+  };
+  cardFetch();
+  addProduitPanier(produit);
+}
